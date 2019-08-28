42f135fc0fad173b2ecbd4746569fe1639db703a
Thiago Zagui Giacomini <thiagozg1995@gmail.com>
2019-08-27 23:47:57 -0300

    Added logic to request Location Permission

--- FindRestaurant/Presentation/LocationPermission/LocationViewController.swift
+++ FindRestaurant/Presentation/LocationPermission/LocationViewController.swift
@@ -11,22 +11,32 @@ import UIKit
 class LocationViewController: UIViewController {
     
     @IBOutlet weak var locationView: LocationView!
+    var locationService: LocationService?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        initLocationClosures()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func initLocationClosures() {
+        locationView.didTapAllow = { [weak self] in
+            self?.locationService?.requestLocationAuthorization()
+        }
+        
+        locationService?.didChangeStatus = { [weak self] success in
+            if success {
+                self?.locationService?.getLocation()
+            }
+        }
+        
+        locationService?.newLocation = { [weak self] result in
+            switch result {
+            case .success(let location):
+                print(location)
+            case .failure(let error):
+                assertionFailure("Error getting the users location \(error)")
+            }
+        }
     }
-    */
 
 }
